2e955a4849915b35de2598e7ebdd87baf2f81d50
Irma <irma.balic@atlantbh.com>
2015-09-23 15:36:28 +0200

    extend from DS.Errors

--- addon/errors.js
+++ addon/errors.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 var get = Ember.get;
 var set = Ember.set;
 
-export default Ember.Object.extend({
+export default DS.Errors.extend({
   unknownProperty: function(property) {
     set(this, property, Ember.A());
     return get(this, property);
